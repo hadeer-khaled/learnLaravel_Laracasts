a4bba11060abf2f9d0a3ecab1b4c402f7815fbb4
Hadeer Khaled <Hadeer_khaled@ETAX.COM.EG>
2024-07-08 01:21:49 +0300

    Ep-22: login  user

--- Demo_1/app/Http/Controllers/SessionController.php
+++ Demo_1/app/Http/Controllers/SessionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -17,9 +18,28 @@ class SessionController extends Controller
     public function store()
     {
         // dd("store logged user");
-        return request()->all();
+        // return request()->all();
         // return view("auth.login");
+
+        $attributes = request()->validate([
+            'email' => ['required'],
+            'password' => ['required' ]
+        ]);
+                
+        if(! Auth::attempt($attributes)){
+            throw ValidationException::withMessages([
+                "email"=> "email or password are wrong",
+                "password"=> "email or password are wrong"
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/posts');
+
     }
+
+
     public function destroy()
     {
        Auth::logout();
