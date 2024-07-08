6e281befe7c55b371f699953accf595ef5643e3f
Hadeer Khaled <Hadeer_khaled@ETAX.COM.EG>
2024-07-08 12:33:37 +0300

    login api

--- Passport_Auth_Demo/app/Http/Controllers/Api/ApiController.php
+++ Passport_Auth_Demo/app/Http/Controllers/Api/ApiController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 use App\Models\User;
+
 class ApiController extends Controller
 {
 
@@ -29,6 +32,34 @@ class ApiController extends Controller
 
     // POST
     public function login (Request $request){
+
+        $validatedAttributes = $request->validate([
+            "email"=>['required', 'email'],
+            "password"=>['required']
+        ]);
+
+        $loginStatus = Auth::attempt($validatedAttributes);
+
+        if($loginStatus){
+            $user = Auth::user();
+            $token = $user->createToken('loginToken')->accessToken ;
+
+            return response()->json([
+                "status"=>200,
+                "message"=>"user logged in successfully",
+                "date"=>$user,
+                "token"=>$token
+            ],200);
+        }
+
+        return response()->json([
+            "status"=>401 ,
+            "message"=>"credentials don't match",
+
+        ],401);
+
+
+
 
     }
 
