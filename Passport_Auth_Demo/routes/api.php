d148da0262844d40bf24356a17f6b8061486111e
Hadeer Khaled <Hadeer_khaled@ETAX.COM.EG>
2024-07-09 09:06:37 +0300

    create custom controller isAdmin

--- Passport_Auth_Demo/routes/api.php
+++ Passport_Auth_Demo/routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
-
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,7 +27,14 @@ Route::post("/login" , [ApiController::class , "login"]);
 
 // Protected Routes
 Route::group(["middleware" => ["auth:api"]],
-                function(){
-                    Route::get("/profile" , [ApiController::class , "profile"]);
-                    Route::get("/logout" , [ApiController::class , "logout"]);
-                });
+            function(){
+                Route::get("/profile" , [ApiController::class , "profile"]);
+                Route::get("/logout" , [ApiController::class , "logout"]);
+
+                Route::apiResource("/posts" , PostController::class);
+                Route::apiResource("/users" , UserController::class)->middleware('admin');
+                // Route::group(['middleware' => ['admin']], function () {
+                //     Route::apiResource("/users", UserController::class);
+                // });
+            });
+
